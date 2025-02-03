b557d8097594e92b072951b4bed1980531f9ef6c
Keity <keityortegam@gmail.com>
2025-02-03 12:57:12 -0500

    Listas

---
+++ listas.py
@@ -0,0 +1,11 @@
+## Listas: es un tipo de dato en Python conformado por una secuencia de elementos de cualquier tipo.
+
+notas = [3.1, 3.5, 2.6]
+print("En tu primer examen obtuviste: ", notas[0])
+
+lista = [1, "hello", 3.1, True]
+print(lista[3])
+
+lista.append(345)
+
+print(lista)
